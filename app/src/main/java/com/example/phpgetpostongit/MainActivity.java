de181d8d6328e3c08fabcded70653258a3a97e96
chaidanGmail <chaidan.ming@gmail.com>
2020-03-08 04:59:44 +0700

    Test branch for sync http request with button- It's not work!!!

--- app/src/main/java/com/example/phpgetpostongit/MainActivity.java
+++ app/src/main/java/com/example/phpgetpostongit/MainActivity.java
@@ -3,6 +3,14 @@ package com.example.phpgetpostongit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void onClick(View view) throws IOException {
+        final TextView textView = (TextView)findViewById(R.id.textView);
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://chaidan2.000webhostapp.com/first.php")
+                .build();
+        Response response = client.newCall(request).execute();
+        final String res = response.body().string();
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                textView.setText(res);
+            }
+        });
     }
 }
