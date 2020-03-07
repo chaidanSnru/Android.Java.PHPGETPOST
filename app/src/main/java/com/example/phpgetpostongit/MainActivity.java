47d9d0c2d6eaff57df2a39a809233ccb41e0e2f8
chaidanGmail <chaidan.ming@gmail.com>
2020-03-08 03:47:15 +0700

    A branch of aysnc Http request with button, it's work !!!

--- app/src/main/java/com/example/phpgetpostongit/MainActivity.java
+++ app/src/main/java/com/example/phpgetpostongit/MainActivity.java
@@ -3,6 +3,17 @@ package com.example.phpgetpostongit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void onClick(View view) {
+        final TextView txtResult = (TextView) findViewById(R.id.textView);
+        // Instantiate new OkHttpClient
+        OkHttpClient client = new OkHttpClient();
+
+        // Initialize a new Request
+        Request request = new Request.Builder()
+                .url("https://chaidan2.000webhostapp.com/first.php")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                // Do something when request failed
+                e.printStackTrace();
+                Log.d("OKhttp", "Request Failed.");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    throw new IOException("Error : " + response);
+                } else {
+                    Log.d("OKhttp", "Request Successful.");
+                }
+
+                // Read data in the worker thread
+                final String data = response.body().string();
+
+                // Display the requested data on UI in main thread
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Display requested url data as string into text view
+                        txtResult.setText(data);
+                    }
+                });
+            }
+        });
     }
 }
