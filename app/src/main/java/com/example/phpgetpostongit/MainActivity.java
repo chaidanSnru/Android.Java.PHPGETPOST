c45976cae0768c8f14ab6c7e26bb693e6e2a5615
chaidanGmail <chaidan.ming@gmail.com>
2020-03-08 03:19:26 +0700

    A branch of aysnc Http request, but it doesn't work

--- app/src/main/java/com/example/phpgetpostongit/MainActivity.java
+++ app/src/main/java/com/example/phpgetpostongit/MainActivity.java
@@ -3,6 +3,17 @@ package com.example.phpgetpostongit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView txtResult = (TextView)findViewById(R.id.resultTxtView);
+        final OkHttpClient asyncClient = new OkHttpClient();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Request asyncRequest = new Request.Builder()
+                        .url("https://chaidan2.000webhostapp.com/first.php")
+                        .build();
+                asyncClient.newCall(asyncRequest).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onResponse(@NotNull Call call, @NotNull final Response response) throws IOException {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    final String res = response.body().string();
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            txtResult.setText(res);
+                                        }
+                                    });
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                    }
+                });
+            }
+        });
+
+
     }
 }
