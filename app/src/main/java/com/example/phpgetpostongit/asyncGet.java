14bc49759f4b52f069b9956719d352a17a0d0073
chaidanGmail <chaidan.ming@gmail.com>
2020-03-07 15:39:06 +0700

    Stop uses separate async class.

--- app/src/main/java/com/example/phpgetpostongit/asyncGet.java
+++ app/src/main/java/com/example/phpgetpostongit/asyncGet.java
@@ -1,5 +1,9 @@
 package com.example.phpgetpostongit;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
@@ -9,8 +13,9 @@ import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 
-public class asyncGet {
+public class asyncGet{
     private OkHttpClient asyncGetClient = new OkHttpClient();
     String asyncRequest(String url) throws IOException {
         Request request = new Request.Builder()
@@ -23,9 +28,12 @@ public class asyncGet {
                 e.printStackTrace(); // not finished yet
             }
 
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
             public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                try (Response response1 = response.body()){} //not finished yet
+                String res = response.body().string();
+                runOnUiThread(new Runnable() {}
+
             }
         });
     }
