0169a4ca6ef47aaf61c05f7ba22a612bcd193af5
chaidanGmail <chaidan.ming@gmail.com>
2020-03-10 06:30:41 +0700

    Test branch for sync http request with button- It's work!!!

--- app/src/main/java/com/example/phpgetpostongit/MainActivity.java
+++ app/src/main/java/com/example/phpgetpostongit/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -19,20 +20,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
-
-    public void onClick(View view) throws IOException {
         final TextView textView = (TextView)findViewById(R.id.textView);
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url("https://chaidan2.000webhostapp.com/first.php")
-                .build();
-        Response response = client.newCall(request).execute();
-        final String res = response.body().string();
-        runOnUiThread(new Runnable() {
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                textView.setText(res);
+            public void onClick(View v) {
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        OkHttpClient client = new OkHttpClient();
+                        Request request = new Request.Builder()
+                                .url("https://chaidan2.000webhostapp.com/first.php")
+                                .build();
+                        try {
+                            Response response = client.newCall(request).execute();
+                            final String txtResponse = response.body().string();
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    textView.setText(txtResponse);
+                                }
+                            });
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                thread.start();
             }
         });
     }
